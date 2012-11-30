df58f462633ff643836877bbf407fd8a6b3c849f
Rodrigo Moreno <rmoreno@codemydream.com>
2012-11-30 15:21:22 -0500

    remove ip before add it to avoid duplicate

--- manualblockips.sh
+++ manualblockips.sh
@@ -5,6 +5,9 @@ BLOCKIPSFILE=IPs.txt
 
 while read line
 do
+  $IPT -D INPUT -s $line -j DROP
+  $IPT -D INPUT -s $line -j LOG --log-prefix "manual ip drop"
+
   $IPT -A INPUT -s $line -j DROP
   $IPT -A INPUT -s $line -j LOG --log-prefix "manual ip drop"
   echo -ne "."
