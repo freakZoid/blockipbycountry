4aa2641bb1db86cb2978d53c45e2bb35f11ff72c
Rodrigo Moreno <rmoreno@codemydream.com>
2012-11-30 14:05:33 -0500

    Singapore country added to be blocked

--- blockipbyCountry.sh
+++ blockipbyCountry.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 ### Block all traffic from AFGHANISTAN (af) and CHINA (CN). Use ISO code ###
-ISO="af cn ps ru th eg"
+ISO="af cn ps ru th eg sg"
  
 ### Set PATH ###
 IPT=/sbin/iptables
