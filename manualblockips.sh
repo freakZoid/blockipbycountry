9f9d6b743d5109af5849c3c0307f59e7222db02e
Rodrigo Moreno <rmoreno@codemydream.com>
2013-12-14 20:53:50 -0500

    Modified script to get ips from TracedIPs.txt

--- manualblockips.sh
+++ manualblockips.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 IPT=/sbin/iptables
-BLOCKIPSFILE=IPs.txt
+BLOCKIPSFILE=TracedIPs.txt
 
 while IFS=$'\n' read -r line || [[ -n "$line" ]];
 do
@@ -10,8 +10,8 @@ do
 			$IPT -D INPUT -s $line -j DROP
 			$IPT -D INPUT -s $line -j LOG --log-prefix "manual ip drop"
 
-			$IPT -A INPUT -s $line -j DROP
-			$IPT -A INPUT -s $line -j LOG --log-prefix "manual ip drop"
+			#$IPT -A INPUT -s $line -j DROP
+			#$IPT -A INPUT -s $line -j LOG --log-prefix "manual ip drop"
   			#echo -ne "."
   			echo $IPT -A INPUT -s $line -j DROP
 		fi
