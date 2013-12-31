8c315cf699e0d16a0921261ed65dc443a7ad60c0
Rodrigo Moreno <rmoreno@codemydream.com>
2013-12-30 20:40:32 -0500

    Script updated to block all ips based on txt

--- manualblockips.sh
+++ manualblockips.sh
@@ -1,21 +1,40 @@
 #!/bin/bash
 
+ISO="TracedIPs.txt china.txt afghanistan.txt"
 IPT=/sbin/iptables
-BLOCKIPSFILE=TracedIPs.txt
+# BLOCKIPSFILE=TracedIPs.txt
 
-while IFS=$'\n' read -r line || [[ -n "$line" ]];
+# while IFS=$'\n' read -r line || [[ -n "$line" ]];
+# do
+	# if [ -n $line ]; then
+		# if [ "$line" != "" ]; then
+			# $IPT -D INPUT -s $line -j DROP
+			# $IPT -D INPUT -s $line -j LOG --log-prefix "manual ip drop"
+
+			# $IPT -A INPUT -s $line -j DROP
+			# $IPT -A INPUT -s $line -j LOG --log-prefix "manual ip drop"
+  			# #echo -ne "."
+  			# echo $IPT -A INPUT -s $line -j DROP
+		# fi
+	# fi
+# done < $BLOCKIPSFILE
+
+for c  in $ISO
 do
-	if [ -n $line ]; then
-		if [ "$line" != "" ]; then
-			$IPT -D INPUT -s $line -j DROP
-			$IPT -D INPUT -s $line -j LOG --log-prefix "manual ip drop"
+	while IFS=$'\n' read -r line || [[ -n "$line" ]];
+	do
+		if [ -n $line ]; then
+			if [ "$line" != "" ]; then
+				$IPT -D INPUT -s $line -j DROP
+				$IPT -D INPUT -s $line -j LOG --log-prefix "manual ip drop"
 
-			$IPT -A INPUT -s $line -j DROP
-			$IPT -A INPUT -s $line -j LOG --log-prefix "manual ip drop"
-  			#echo -ne "."
-  			echo $IPT -A INPUT -s $line -j DROP
+				$IPT -A INPUT -s $line -j DROP
+				$IPT -A INPUT -s $line -j LOG --log-prefix "manual ip drop"
+				#echo -ne "."
+				echo $IPT -A INPUT -s $line -j DROP
+			fi
 		fi
-	fi
-done < $BLOCKIPSFILE
+	done < $c
+done
 
 /etc/init.d/iptables save
